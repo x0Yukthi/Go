8b20fb49cd1f0570afabc47b4500b2a71726b214
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2026-04-05 22:30:25 -0700

    handler

---
+++ handler.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/miekg/dns"
+
+func handleQuery(w dns.ResponseWriter, r *dns.Msg) {
+
+	name := r.Question[0].Name
+	command, location := parseQuery(name)
+	var answer string
+	if command == "time" {
+		answer = getTime(location)
+	} else {
+		answer = "error: unknown command"
+	}
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Answer = append(m.Answer, &dns.TXT{
+		Hdr: dns.RR_Header{
+			Name:   r.Question[0].Name,
+			Rrtype: dns.TypeTXT,
+			Class:  dns.ClassINET,
+			Ttl:    30,
+		},
+		Txt: []string{answer},
+	})
+	w.WriteMsg(m)
+
+}
