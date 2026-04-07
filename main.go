65e7f5bc7aad383dccbf3ff1be031ae78eca4529
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2026-04-06 23:29:06 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,35 +3,9 @@ package main
 import (
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/miekg/dns"
 )
-
-var timezones = map[string]string{
-	"london":     "Europe/London",
-	"paris":      "Europe/Paris",
-	"tokyo":      "Asia/Tokyo",
-	"india":      "Asia/Kolkata",
-	"california": "America/Los_Angeles",
-	"new york":   "America/New_York",
-	"dubai":      "Asia/Dubai",
-	"sydney":     "Australia/Sydney",
-	"singapore":  "Asia/Singapore",
-}
-
-func getTime(location string) string {
-
-	if tz, ok := timezones[location]; ok {
-		loc, err := time.LoadLocation(tz)
-		if err != nil {
-			return "error: bad timezone"
-		}
-		return time.Now().In(loc).Format("15:04:05 MST, Mon 02 Jan 2006")
-	}
-	return "error: location not supported"
-
-}
 
 func parseQuery(domain string) (command, location string) {
 	domain = strings.TrimSuffix(domain, ".")
@@ -58,11 +32,11 @@ func main() {
 	dns.HandleFunc(".", handleQuery)
 
 	server := &dns.Server{
-		Addr: ":8053",
+		Addr: ":5053",
 		Net:  "udp",
 	}
 
-	fmt.Println("DNS server running on :8053")
+	fmt.Println("DNS server running on :5053")
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("error:", err)
